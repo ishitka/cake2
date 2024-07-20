08a9678198b39b02c54be93e341d29f672be8514
EC2 Default User <ec2-user@ip-172-31-21-85.ap-northeast-1.compute.internal>
2024-07-20 12:51:56 +0000

    add

--- app/controllers/admin/customers_controller.rb
+++ app/controllers/admin/customers_controller.rb
@@ -3,4 +3,25 @@ class Admin::CustomersController < ApplicationController
   def index
     @customers = Customer.all
   end
+  
+  def show
+    @customer = Customer.find(params[:id])
+  end
+  
+  def edit
+    @customer = Customer.find(params[:id])
+  end
+  
+  def update
+    @customer = Customer.find(params[:id])
+    @customer.update(customer_params)
+    redirect_to admin_customer_path(@customer.id)
+  end
+    
+  
+  private
+  
+  def customer_params
+    params.require(:customer).permit(:first_name, :last_name, :first_name_kana, :last_name_kane, :postal_code, :address, :telephone_number, :email, :password, :password_confirmation, :is_active)
+  end
 end
--- app/controllers/public/addresses_controller.rb
+++ app/controllers/public/addresses_controller.rb
@@ -1,2 +1,39 @@
 class Public::AddressesController < ApplicationController
+  
+  def index
+    customer_id = current_customer.id
+    @addresses = Address.where(customer_id: customer_id)
+  end
+  
+  def create
+    @address = Address.new(address_params)
+    @address.customer_id = current_customer.id
+    if @address.save
+      redirect_to addresses_path
+    else
+      render :new
+    end
+  end
+  
+  def edit
+    @address = Address.find(params[:id])
+  end
+  
+  def update
+    @address = Address.find(params[:id])
+    @address.update(address_params)
+    redirect_to addresses_path(current_customer.id)
+  end
+  
+  def destroy
+    @address = Address.find(params[:id])
+    @address.destroy
+    redirect_to addresses_path(current_customer.id)
+  end
+  
+  private
+  
+  def address_params
+    params.require(:address).permit(:postal_code, :address, :name, :customer_id)
+  end
 end
